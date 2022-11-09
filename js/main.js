5c0ed77a618c530c0ab68c5f36ab4c8f3a179243
Ira <ira.zvonova@outlook.com>
2022-11-09 17:58:36 +0300

    feat(app): finish-commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,62 @@
-$(function(){
-    $('.header-slider').slick({
-        dots: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
-        vertical: true
-    });
+$(function () {
+  $('.header-slider').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
+    vertical: true,
+    responsive: [
+      {
+        breakpoint: 371,
+        settings: {
+          dots: false,
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 2000
+        }
+      }
+    ]
+  });
+
+
+  $('.product__name').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    asNavFor: '.product__content',
+    vertical: true,
+    prevArrow: '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 891,
+        settings: {
+          vertical: false,
+          slidesToShow: 3,
+          arrows: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 461,
+        settings: {
+          vertical: false,
+          slidesToShow: 1,
+          arrows: false,
+          dots: true
+        }
+      }
+    ]
+  });
+  $('.product__content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.product__name',
+    fade: true,
+    arrows: false
+  });
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
 });
